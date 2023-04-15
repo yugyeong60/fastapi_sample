40cc9cb3f165d11018bf6629ef6da7680de05fcc
kdpark <kdpark@kw.ac.kr>
2023-04-16 00:52:15 +0900

    first fastapi project

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from todo import todo_router # added
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome() -> dict:
+    return {
+        "msg" : "hello world?"
+    }
+
+app.include_router(todo_router)  # added
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+
+
---
+++ todo.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Path
+from model import Todo
+
+todo_router = APIRouter()
+
+todo_list = []
+
+@todo_router.post("/todo")
+async def add_todo(todo: Todo) -> dict:
+    todo_list.append(todo)
+    return {
+        "msg" : "todo added successfully"
+    }
+
+@todo_router.get("/todo")
+async def retrieve_todos() -> dict:
+    return {
+        "todos" : todo_list
+    }
+
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(todo_id: int = Path(..., title = "the ID of the todo to retrieve")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return { "todo" : todo}
+    return {"msg" : "todo with supplied ID doesn't exist"}
