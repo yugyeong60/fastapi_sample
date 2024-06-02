aaf4e4d2da59d1d671c9241ae0f9d09d1c851c9a
yugyeong60 <149192668+yugyeong60@users.noreply.github.com>
2024-06-02 22:36:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ async def welcome() -> dict:
 app.include_router(todo_router)  # added
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
